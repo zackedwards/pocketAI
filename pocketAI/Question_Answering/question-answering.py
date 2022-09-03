c4b6200dc5c146e5461848088abf080f1b436134
DomeAustinB <100167612+DomeAustinB@users.noreply.github.com>
2022-09-02 20:41:18 -0400

    question answering mod

--- pocketAI/Question_Answering/question-answering.py
+++ pocketAI/Question_Answering/question-answering.py
@@ -9,31 +9,32 @@ def open_file(filepath):
 
 
 def save_convo(text, query):
-    with open('pocketAI/Text_Generator/generations/%s_%s.txt' % (query, time()), 'w', encoding='utf-8') as outfile:
+    with open('pocketAI/Question_Answering/answers/%s_%s.txt' % (query, time()), 'w', encoding='utf-8') as outfile:
         outfile.write(text)
 
 
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt="Brainstorm some ideas combining VR and fitness:\n\n", engine='text-davinci-002', temperature=0.6, top_p=1, max_tokens=1000, frequency_penalty=1, presence_penalty=1):
+def gpt3_completion(prompt="What is the human life expectancy in the United States?\n\n", engine='text-davinci-002', temperature=0, top_p=1, max_tokens=100, frequency_penalty=0, presence_penalty=0, best_of=1):
     max_retry = 5
     retry = 0
     while True:
         try:
             response = openai.Completion.create(
                 model="text-davinci-002",
-                prompt="Brainstorm some ideas combining VR and fitness:\n\n",
-                temperature=0.6,
-                max_tokens=1000,
+                prompt="What is the human life expectancy in the United States?\n\n",
+                temperature=0,
+                max_tokens=100,
                 top_p=1,
-                frequency_penalty=1,
-                presence_penalty=1
+                frequency_penalty=0,
+                presence_penalty=0,
+                best_of=1
                 )
             text = response['choices'][0]['text'].strip()
             filename = '%s_gpt3.txt' % time()
-            with open('pocketAI/Text_Generator/gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write(prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+            with open('pocketAI/Question_Answering/gpt3_logs/%s' % filename, 'w') as outfile:
+                outfile.write(prompt + '\n\n==========\n\nANSWER:\n\n' + text)
             return text
         except Exception as oops:
             retry += 1
@@ -44,16 +45,14 @@ def gpt3_completion(prompt="Brainstorm some ideas combining VR and fitness:\n\n"
 
 
 if __name__ == '__main__':
-    for i in range(0, 4):
-        queries = open_file('pocketAI/Text_Generator/query.txt').splitlines()
+    for i in range(0, 3):
+        queries = open_file('pocketAI/Question_Answering/question.txt').splitlines()
         for query in queries:
             print(query)
-            prompt = open_file('pocketAI/Text_Generator/prompt.txt').replace('<<QUERY>>', query)
+            prompt = open_file('pocketAI/Question_Answering/prompt.txt').replace('<<QUERY>>', query)
             response = gpt3_completion(prompt)
             outtext = '%s' % response
             print(outtext)
             qry = query.replace(' ', '')[0:15]
             save_convo(outtext, qry)
             #exit()
-
-
